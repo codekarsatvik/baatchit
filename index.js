20b4f6ec3e27c090d889a9f3121afbf1cbc89476
satvik aggarwal <skaggarwal2001.com@gmail.com>
2020-07-21 18:48:03 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+// this willhandkle socketio server
+const io = require('socket.io')(8000);
+
+const users = {};
+
+io.on('connection', socket =>{
+  socket.on('new-user-joined', name=>{
+    console.log(name);
+     users[socket.id]= name;
+     socket.broadcast.emit('user-joined',name)
+  });
+  socket.on('send',message=>{
+
+    socket.broadcast.emit('recieve',{message: message , name :users[socket.id]})
+  });
+
+  socket.on('disconnect', message =>{
+    socket.broadcast.emit('chat-leaved',users[socket.id]);
+    delete users[socket.id];
+
+  });
+});
---
+++ js/index.js
@@ -0,0 +1,57 @@
+var audio = new Audio('js/ting.mp3');
+const socket = io('http://localhost:8000');
+audio.play();
+
+
+const form = document.getElementById('send-form');
+const messageinput = document.getElementById('messageinp');
+const messagecontainer = document.querySelector('.chatbox');
+
+const append = (message,position)=>{
+  var div = document.createElement('div');
+  var messagetext = document.createElement('h1');
+  div.appendChild(messagetext);
+
+  var text = document.createTextNode(message);
+  messagetext.classList.add("message",position)
+  messagetext.appendChild(text);
+  messagecontainer.appendChild(messagetext);
+  if(position!='right'){
+    audio.play();
+    muted = "muted";
+
+
+  }
+
+}
+
+const name = prompt('enter your name to join ');
+socket.emit('new-user-joined',name);
+socket.on('user-joined', (name) =>{
+  append(  name + ' has joined the chat', 'middle');
+})
+
+form.addEventListener('submit', (e)=>{
+  e.preventDefault();
+  var message = document.querySelector('#messageinp').value;
+  append("you : " + message ,'right');
+  socket.emit('send', message );
+  messageinput.value ='';
+
+});
+socket.on('recieve', data =>{
+  console.log({});
+  append( data.name + " : " + data.message , 'left');
+});
+socket.on('chat-leaved', data =>{
+
+  append( data + " has left the Chat !", 'middle');
+});
+
+
+
+
+
+
+
+// button
